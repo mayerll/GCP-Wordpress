ebe91c9a620b8ac2ec83386f5424d5f3f151e291
mayerll <mayerlbp@hotmail.com>
2024-01-14 13:53:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 git config --global user.email "mayerlbp@hotmail.com"
 git config --global user.name "mayerll"
 
-git push https://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@github.com/mayerll/GCP-Wordpress.git
-
